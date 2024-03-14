b3dfafa04d3aed664d95f42a045a87befba563dd
Mike Lischke <mike.lischke@oracle.com>
2024-03-14 21:46:41 +0100

    Added all MySQL benchmark sources
    All target benchmarks build and run except for the cpp-wasm version, which uses a different path in the generated parser + lexer, that must be adjusted manually atm.

---
+++ src/mysql/generate.sh
@@ -0,0 +1,26 @@
+# Generates the MySQL parser + lexer files for the benchmarks.
+
+# C++
+java -jar ../../antlr/antlr-4.13.1-complete.jar \
+    -Dlanguage=Cpp -o ./targets/antlr4-cpp -visitor -listener -package parsers -Xexact-output-dir -Werror \
+    ./targets/antlr4-cpp/MySQLLexer.g4 ./targets/antlr4-cpp/MySQLParser.g4
+
+# WebAssembly
+java -jar ../../antlr/antlr4-4.13.1-SNAPSHOT-complete.jar \
+    -Dlanguage=TypeScript -o ./targets/antlr4-cpp-wasm -visitor -listener -package antlr4 -Xexact-output-dir -Werror \
+    ./targets/antlr4-cpp-wasm/MySQLLexer.g4 ./targets/antlr4-cpp-wasm/MySQLParser.g4
+
+# Old TypeScript
+java -jar ../../antlr/antlr-4.13.1-complete.jar \
+    -Dlanguage=TypeScript -o ./targets/antlr4 -visitor -listener -package antlr4 -Xexact-output-dir -Werror \
+    ./targets/antlr4/MySQLLexer.g4 ./targets/antlr4/MySQLParser.g4
+
+# New TypeScript
+antlr4ng \
+    -Dlanguage=TypeScript -o ./targets/antlr4ng -visitor -listener -package antlr4 -Xexact-output-dir -Werror \
+    ./targets/antlr4ng/MySQLLexer.g4 ./targets/antlr4ng/MySQLParser.g4
+
+# antlr4ts
+antlr4ts \
+    -o ./targets/antlr4ts -visitor -listener -package antlr4 -Xexact-output-dir -Werror \
+    ./targets/antlr4ts/MySQLLexer.g4 ./targets/antlr4ts/MySQLParser.g4
